f7e49f6e3481a6eb2c046abfeb0ca61da6ad7538
leejaejae <leesengjie@naver.com>
2022-06-02 11:16:20 +0900

    test

--- app/src/main/java/org/nyeong/lipo/SplashActivity.java
+++ app/src/main/java/org/nyeong/lipo/SplashActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class SlashActivity extends Activity {
+public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         try{
@@ -19,3 +19,5 @@ public class SlashActivity extends Activity {
         finish();
     }
 }
+
+
