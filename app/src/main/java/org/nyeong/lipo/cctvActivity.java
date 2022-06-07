3e791611049201bba7db9f90946dc7436d47ee40
leejaejae <leesengjie@naver.com>
2022-06-07 21:30:33 +0900

    버튼 수정

--- app/src/main/java/org/nyeong/lipo/cctvActivity.java
+++ app/src/main/java/org/nyeong/lipo/cctvActivity.java
@@ -11,34 +11,23 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 01dffcb26f9b8763c5501159f30e21d98f2ffe61
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
-<<<<<<< HEAD
->>>>>>> 37b900c18be6262f4cc7e19bc19d6c62961d8018
-=======
-
-<<<<<<< HEAD
-=======
->>>>>>> 01dffcb26f9b8763c5501159f30e21d98f2ffe61
-
->>>>>>> e0d32f436b5b978f27610d34e871f865252f935a
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import java.io.IOException;
 
 public class cctvActivity extends AppCompatActivity {
     final String TAG = "TAG+CCTVFragment";
-    ImageButton callpolice, recode, warning;
+    ImageButton callpolice, warning;
 //    WebView webView;
     WebSettings webSettings;
-//    TextView callText;
+    boolean i = true;
 
     @SuppressLint({"ClickableViewAccessibility", "SetJavaScriptEnabled"})
     @Override
@@ -49,10 +38,7 @@ public class cctvActivity extends AppCompatActivity {
         Log.d(TAG, "Create CCTV Fragment");
 
 //        webView = (WebView) findViewById(R.id.cctvWeb);
-//        callText = (TextView) findViewById(R.id.callText);
-//        outhome = (ToggleButton) findViewById(R.id.outhome);
         callpolice = (ImageButton) findViewById(R.id.callpolice);
-        recode = (ImageButton) findViewById(R.id.recode);
         warning = (ImageButton) findViewById(R.id.warning);
 
 //        webSettings = webView.getSettings();
@@ -73,63 +59,72 @@ public class cctvActivity extends AppCompatActivity {
 //            }
 //        }); // WebView 터치 시 새로고침
 
-//        @Override
-//        outhome.setOnClickListener(new View.setOnClickListener() {
-////            @Override
-//            public void onCheckedChanged(View buttonView, boolean isChecked) {
-//                if(isChecked) {
-//                    Toast.makeText(getApplicationContext(), "checked", Toast.LENGTH_LONG).show();
-//                }
-//                else {
-//                    Toast.makeText(getApplicationContext(), "unchecked", Toast.LENGTH_LONG).show();
-//                }
-//            }
-//        }); // 외출모드
+        final TextView clock = (TextView) findViewById(R.id.clock);
 
-        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.outhome);
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    Toast.makeText(getApplicationContext(), "외출 중", Toast.LENGTH_SHORT).show();
+        (new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                while (!Thread.interrupted())
+                    try{
+                        Thread.sleep(1000);
+                        runOnUiThread(new Runnable(){
+                            @Override
+                            public void run()
+                            {
+                                clock.setText(getCurrentTime());
+                            }
+                        });
+                    }
+                    catch (InterruptedException e)
+                    {
+
+                    }
+            }
+        })).start();
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.out_home_home);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (toggleButton.isChecked() && i==true) {
+                    toggleButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.home));
+                    i = false;
                 } else {
-                    Toast.makeText(getApplicationContext(), "집!", Toast.LENGTH_SHORT).show();
+                    toggleButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.out_home));
+                    i = true;
                 }
             }
-        });
+        }); // 외출모드
+
+//        out_home_home.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+////                        try {
+////                            ((MainActivity)MainActivity.context).tcpThread.cctvOff();
+////                        } catch (IOException e) {
+////                            e.printStackTrace();
+////                        }
+//                    }
+//                }).start();
+//            }
+//        }); // 경고음 출력
+
+        ToggleButton toggleButton2 = (ToggleButton) findViewById(R.id.recode);
+        toggleButton2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(getApplicationContext(), "녹화 중", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "정지", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });  // 녹화
 
         callpolice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-//                        try {
-//                            ((MainActivity)MainActivity.context).tcpThread.cctvOff();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-                    }
-                }).start();
-            }
-        }); // 경찰 부르기
-
-        warning.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-//                        try {
-//                            ((MainActivity)MainActivity.context).tcpThread.cctvOff();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-                    }
-                }).start();
-            }
-        }); // 경고음 출력
-
-        recode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(cctvActivity.this);
@@ -152,6 +147,29 @@ public class cctvActivity extends AppCompatActivity {
                 alertDialog.show();
             }
         }); // 신고하기 버튼 클릭 시 112 전화걸기로 이동
+
+        warning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        try {
+//                            ((MainActivity)MainActivity.context).tcpThread.cctvOff();
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+                    }
+                }).start();
+            }
+        }); // 경고음 출력
+
+    }
+    public String getCurrentTime(){
+        long time = System.currentTimeMillis();
+        SimpleDateFormat dayTime = new SimpleDateFormat("MM-dd hh:mm");
+        String str = dayTime.format(new Date(time));
+        return str;
     }
 }
 
